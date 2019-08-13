5f5a183726f78e6757982c1719dcaf5463855fc2
positivecse <47621096+positivecse@users.noreply.github.com>
2019-08-13 22:30:26 +0530

    Update Armstrongintervals.c

--- Armstrongintervals.c
+++ Armstrongintervals.c
@@ -1,27 +1,20 @@
-#include<stdio.h>
-#include<conio.h>
-int main()
+void main()
 {
-int n1,n2,result=0,i,temp1,temp2,n=0,r;
+int n1,n2,i,t,sum,r;
 clrscr();
 printf("Enter any two numbers(interval):");
 scanf("%d%d",&n1,&n2);
 printf("Armstrong  numbers between %d and %d are:",n1,n2);
 for(i=n1+1;i<n2;++i)
 {
-temp2=i;temp1=i;
-while(temp1!=0)
-{temp1=temp1/10;++n;}
-while(temp2!=0)
-{r=temp2%10;
-result=result+pow(r,n);
-temp2=temp2/10;
+t=i;
+sum=0;
+while(t!=0)
+{r=t%10;
+sum=sum+r*r*r;
+t=t/10;
 }
-temp2=i;
-temp1=i;
-if(result==i)
-{printf("\t%d",i);}
-n=0;
-result=0; }
+if(i==sum)
+{printf("\t%d",i);} }
 getch();
 }
